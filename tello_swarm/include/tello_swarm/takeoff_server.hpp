d49d3de738e1dbffbf71ba77f70680bbf6d0f3f1
minh <minhnhangia@gmail.com>
2025-12-12 16:09:42 +0800

    refactor takeoff_server and landing_server to use a common base class to reduce repetition

--- tello_swarm/include/tello_swarm/takeoff_server.hpp
+++ tello_swarm/include/tello_swarm/takeoff_server.hpp
@@ -1,50 +1,21 @@
 #ifndef TELLO_SWARM__TAKEOFF_SERVER__HPP_
 #define TELLO_SWARM__TAKEOFF_SERVER__HPP_
 
-#include <string>
-#include <vector>
-#include <map>
-#include <future>
-
-#include "rclcpp/rclcpp.hpp"
-#include "std_srvs/srv/trigger.hpp"
+#include "tello_swarm/swarm_trigger_base.hpp"
 
 namespace tello_swarm
 {
 
-class TakeoffServer : public rclcpp::Node
+/**
+ * @brief Takeoff server for coordinating swarm takeoff operations
+ * 
+ * Thin wrapper around SwarmTriggerBase that provides takeoff-specific
+ * configuration. Exposes /takeoff_all service that triggers /takeoff on all drones.
+ */
+class TakeoffServer : public SwarmTriggerBase
 {
 public:
     TakeoffServer();
-
-private:
-    // Parameters
-    std::vector<std::string> drone_ids_;
-    std::string takeoff_service_name_;
-    double client_timeout_sec_;
-
-    // Service server to trigger swarm takeoff
-    rclcpp::Service<std_srvs::srv::Trigger>::SharedPtr takeoff_all_srv_;
-
-    // Persistent service clients for each drone
-    std::map<std::string, rclcpp::Client<std_srvs::srv::Trigger>::SharedPtr> clients_;
-
-    // Reentrant callback group for parallel service handling
-    rclcpp::CallbackGroup::SharedPtr reentrant_callback_group_;
-
-    void create_service_clients();
-
-    void print_startup_summary();
-
-    void handle_takeoff_all(
-        const std::shared_ptr<std_srvs::srv::Trigger::Request> request,
-        std::shared_ptr<std_srvs::srv::Trigger::Response> response);
-
-    // Async service call helper
-    std::shared_future<std::shared_ptr<std_srvs::srv::Trigger::Response>>
-    call_single_drone_takeoff_async(
-        const std::string &id,
-        const rclcpp::Client<std_srvs::srv::Trigger>::SharedPtr &client);
 };
 
 } // namespace tello_swarm
